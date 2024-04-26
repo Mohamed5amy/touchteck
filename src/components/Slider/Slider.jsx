70b41b984f457eae38f6908fbca458d7d8edc3cf
mohamed-samy <mohameddiv58@gmail.com>
2024-04-26 09:13:10 +0300

    slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -9,7 +9,7 @@ import { Stack } from "@mui/material";
 
 const Slider = ({slides}) => {
   return (
-    <Stack position={"relative"} width={"100%"} height={{xs : "auto" , sm : "525px"}}>
+    <Stack position={"relative"} width={"100%"} height={{xs : "200px" , md : "525px"}}>
       <Swiper
         pagination={{clickable : true}} 
         modules={[Pagination ,Autoplay]} 
@@ -27,7 +27,7 @@ const Slider = ({slides}) => {
             return (
               <SwiperSlide key={i}>
                 <a href={slide.title} target='_blank' rel="noreferrer">
-                  <Stack width={"100%"} height={{xs : "400px" , md : "525px"}}>
+                  <Stack width={"100%"} height={{xs : "200px" , md : "525px"}}>
                     <img src={import.meta.env.VITE_LINK + slide.image} alt="slider image" width={"100%"} height={"100%"} style={{objectFit : "fill"}} />
                   </Stack>
                 </a>
